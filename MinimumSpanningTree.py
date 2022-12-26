7260be374d15d937a47b9474886603801fcd5e5e
Pooyan Oskouhi <117625732+pooyanosk1382@users.noreply.github.com>
2022-12-27 00:50:18 +0330

    Add files via upload

---
+++ MinimumSpanningTree.py
@@ -0,0 +1,265 @@
+import sys
+
+
+# -----------------------Kruskal-----------------------------
+class GraphKruskal:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # Search function
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def apply_union(self, parent, rank, x, y):
+        xRoot = self.find(parent, x)
+        yRoot = self.find(parent, y)
+        if rank[xRoot] < rank[yRoot]:
+            parent[xRoot] = yRoot
+        elif rank[xRoot] > rank[yRoot]:
+            parent[yRoot] = xRoot
+        else:
+            parent[yRoot] = xRoot
+            rank[xRoot] += 1
+
+    #  Applying Kruskal algorithm
+    def kruskal_algo(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        for u, v, weight in result:
+            print("%d - %d: %d" % (u, v, weight))
+
+
+g = GraphKruskal(6)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 2)
+g.add_edge(1, 0, 4)
+g.add_edge(2, 0, 4)
+g.add_edge(2, 1, 2)
+g.add_edge(2, 3, 3)
+g.add_edge(2, 5, 2)
+g.add_edge(2, 4, 4)
+g.add_edge(3, 2, 3)
+g.add_edge(3, 4, 3)
+g.add_edge(4, 2, 4)
+g.add_edge(4, 3, 3)
+g.add_edge(5, 2, 2)
+g.add_edge(5, 4, 3)
+g.kruskal_algo()
+# -----------------------------------------------------------
+
+
+# -----------------------Boruvka-----------------------------
+class GraphBoruvka:
+
+    def __init__(self, vertices):
+        self.V = vertices  # No. of vertices
+        self.graph = []  # default dictionary to store graph
+
+    # function to add an edge to graph
+    def addEdge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # A utility function to find set of an element i
+    # (uses path compression technique)
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    # A function that does union of two sets of x and y
+    # (uses union by rank)
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of high rank tree
+        # (Union by Rank)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        # If ranks are same, then make one as root and increment
+        # its rank by one
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    # The main function to construct MST using Kruskal's algorithm
+    def boruvkaMST(self):
+        parent = []
+        rank = []
+
+        # An array to store index of the cheapest edge of
+        # subset. It store [u,v,w] for each component
+        cheapest = []
+
+        # Initially there are V different trees.
+        # Finally there will be one tree that will be MST
+        numTrees = self.V
+        MSTWeight = 0
+
+        # Create V subsets with single elements
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+            cheapest = [-1] * self.V
+
+        # Keep combining components (or sets) until all
+        # components are not combined into single MST
+
+        while numTrees > 1:
+
+            # Traverse through all edges and update
+            # cheapest of every component
+            for i in range(len(self.graph)):
+
+                # Find components (or sets) of two corners
+                # of current edge
+                u, v, w = self.graph[i]
+                set1 = self.find(parent, u)
+                set2 = self.find(parent, v)
+
+                # If two corners of current edge belong to
+                # same set, ignore current edge. Else check if
+                # current edge is closer to previous
+                # cheapest edges of set1 and set2
+                if set1 != set2:
+
+                    if cheapest[set1] == -1 or cheapest[set1][2] > w:
+                        cheapest[set1] = [u, v, w]
+
+                    if cheapest[set2] == -1 or cheapest[set2][2] > w:
+                        cheapest[set2] = [u, v, w]
+
+            # Consider the above picked cheapest edges and add them
+            # to MST
+            for node in range(self.V):
+
+                # Check if cheapest for current set exists
+                if cheapest[node] != -1:
+                    u, v, w = cheapest[node]
+                    set1 = self.find(parent, u)
+                    set2 = self.find(parent, v)
+
+                    if set1 != set2:
+                        MSTWeight += w
+                        self.union(parent, rank, set1, set2)
+                        print("Edge %d-%d with weight %d included in MST" % (u, v, w))
+                        numTrees = numTrees - 1
+
+            # reset cheapest array
+            cheapest = [-1] * self.V
+
+        print("Weight of MST is %d" % MSTWeight)
+
+
+g = GraphBoruvka(4)
+g.addEdge(0, 1, 10)
+g.addEdge(0, 2, 6)
+g.addEdge(0, 3, 5)
+g.addEdge(1, 3, 15)
+g.addEdge(2, 3, 4)
+g.boruvkaMST()
+# -----------------------------------------------------------
+
+
+# -------------------------Prim------------------------------
+class GraphPrim:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
+
+    # A utility function to print the constructed MST stored in parent[]
+    def printMST(self, parent):
+        print("Edge \tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minKey(self, key, mstSet):
+
+        # Initialize min value
+        minimum = sys.maxsize
+        min_index = -1
+
+        for v in range(self.V):
+            if key[v] < minimum and mstSet[v] is False:
+                minimum = key[v]
+                min_index = v
+
+        return min_index
+
+    # Function to construct and print MST for a graph
+    # represented using adjacency matrix representation
+    def primMST(self):
+
+        # Key values used to pick minimum weight edge in cut
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V  # Array to store constructed MST
+        # Make key 0 so that this vertex is picked as first vertex
+        key[0] = 0
+        mstSet = [False] * self.V
+
+        parent[0] = -1  # First node is always the root of
+
+        for items in range(self.V):
+
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minKey(key, mstSet)
+
+            # Put the minimum distance vertex in
+            # the shortest path tree
+            mstSet[u] = True
+
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for v in range(self.V):
+
+                # graph[u][v] is non zero only for adjacent vertices of m
+                # mstSet[v] is false for vertices not yet included in MST
+                # Update the key only if graph[u][v] is smaller than key[v]
+                if 0 < self.graph[u][v] < key[v] and mstSet[v] is False:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        self.printMST(parent)
+
+
+# Driver's code
+g = GraphPrim(5)
+g.graph = [[0, 2, 0, 6, 0],
+           [2, 0, 3, 8, 5],
+           [0, 3, 0, 0, 7],
+           [6, 8, 0, 0, 9],
+           [0, 5, 7, 9, 0]]
+g.primMST()
+# -----------------------------------------------------------
